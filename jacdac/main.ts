09000e8c6c7e0a9032e4096bade7d5b95d3185cb
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-04-07 22:08:33 -0700

    added jacdac project

---
+++ jacdac/main.ts
@@ -0,0 +1,44 @@
+//% deprecated
+namespace Kitronik_LAMPbit {
+
+}
+
+namespace servers {
+    class LightBulbServer extends jacdac.Server {
+        brightness: number
+        constructor() {
+            super("", jacdac.SRV_LIGHT_BULB)
+            this.brightness = 0
+        }
+
+        handlePacket(pkt: jacdac.JDPacket) {
+            this.handleRegBool(pkt, jacdac.LightBulbReg.Dimmable, false)
+            const r = this.handleRegFormat(pkt, jacdac.LightBulbReg.Brightness, "u0.16", [this.brightness])
+            this.brightness = Math.round(r[0])
+
+            const display = this.brightness > 0 ? Kitronik_LAMPbit.DisplayLamp.On : Kitronik_LAMPbit.DisplayLamp.Off
+            Kitronik_LAMPbit.lampLightLED(display)
+        }
+    }
+
+    jacdac.startSelfServers(() => [
+        jacdac.createSimpleSensorServer("",
+            jacdac.SRV_LIGHT_LEVEL, "u0.16",
+            () => Kitronik_LAMPbit.lightLevel() / 1023),
+        new LightBulbServer()
+    ])
+}
+
+namespace modules {
+    /**
+     * The ambient light level measured by Lamp:bit
+     */
+    //% fixedInstance
+    export const kitronikLampBitLightLevel = new LightLevelClient("kitronik Lamp:bit light level?device=self")
+
+    /**
+     * The lamp controller
+     */
+    //% fixedInstance
+    export const kitronikLampBitLamp = new LightBulbClient("kitronik lamp:bit lamp?device=self")
+}
